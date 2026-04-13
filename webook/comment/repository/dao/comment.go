f0ba8960523ad495283b945b23851b1a192cfa59
WWW <31413241324.com>
2026-04-13 15:05:21 +0800

    评论系统

---
+++ webook/comment/repository/dao/comment.go
@@ -0,0 +1,117 @@
+package dao
+
+import (
+	"context"
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+// ErrDataNotFound 通用的数据没找到
+var ErrDataNotFound = gorm.ErrRecordNotFound
+
+type Comment struct {
+	Id int64 `gorm:"column:id;primaryKey" json:"id"`
+	// 发表评论的用户
+	Uid int64 `gorm:"column:uid;index" json:"uid"`
+	// 发表评论的业务类型
+	Biz string `gorm:"column:biz;index:biz_type_id" json:"biz"`
+	// 对应的业务ID
+	BizID int64 `gorm:"column:biz_id;index:biz_type_id" json:"bizID"`
+	// 根评论为0表示一级评论
+	RootID sql.NullInt64 `gorm:"column:root_id;index" json:"rootID"`
+	// 父级评论
+	PID sql.NullInt64 `gorm:"column:pid;index" json:"pid"`
+	// 外键 用于级联删除
+	ParentComment *Comment `gorm:"ForeignKey:PID;AssociationForeignKey:ID;constraint:OnDelete:CASCADE"`
+	// 评论内容
+	Content string `gorm:"type:text;column:content" json:"content"`
+	// 创建时间
+	Ctime int64 `gorm:"column:ctime;" json:"ctime"`
+	// 更新时间
+	Utime int64 `gorm:"column:utime;" json:"utime"`
+}
+
+func (*Comment) TableName() string {
+	return "comments"
+}
+
+type GORMCommentDAO struct {
+	db *gorm.DB
+}
+
+func (c *GORMCommentDAO) FindRepliesByRid(ctx context.Context,
+	rid int64, id int64, limit int64) ([]Comment, error) {
+	var res []Comment
+	err := c.db.WithContext(ctx).
+		Where("root_id = ? AND id > ?", rid, id).
+		Order("id ASC").
+		Limit(int(limit)).Find(&res).Error
+	return res, err
+}
+
+func NewCommentDAO(db *gorm.DB) CommentDAO {
+	return &GORMCommentDAO{
+		db: db,
+	}
+}
+
+func (c *GORMCommentDAO) FindOneByIDs(ctx context.Context, ids []int64) ([]Comment, error) {
+	var res []Comment
+	err := c.db.WithContext(ctx).
+		Where("id in ?", ids).
+		First(&res).
+		Error
+	return res, err
+}
+
+func (c *GORMCommentDAO) FindByBiz(ctx context.Context, biz string,
+	bizId, minID, limit int64) ([]Comment, error) {
+	var res []Comment
+	err := c.db.WithContext(ctx).
+		Where("biz = ? AND biz_id = ? AND id < ? AND pid IS NULL", biz, bizId, minID).
+		Limit(int(limit)).
+		Find(&res).Error
+	return res, err
+}
+
+// FindRepliesByPid 查找评论的直接评论
+func (c *GORMCommentDAO) FindRepliesByPid(ctx context.Context,
+	pid int64,
+	offset,
+	limit int) ([]Comment, error) {
+	var res []Comment
+	err := c.db.WithContext(ctx).Where("pid = ?", pid).
+		Order("id DESC").
+		Offset(offset).Limit(limit).Find(&res).Error
+	return res, err
+}
+
+func (c *GORMCommentDAO) Insert(ctx context.Context, u Comment) error {
+	return c.db.
+		WithContext(ctx).
+		Create(u).
+		Error
+}
+
+func (c *GORMCommentDAO) FindCommentList(ctx context.Context, u Comment) ([]Comment, error) {
+	var res []Comment
+	builder := c.db.WithContext(ctx)
+	if u.Id == 0 {
+		builder = builder.
+			Where("biz=?", u.Biz).
+			Where("biz_id=?", u.BizID).
+			Where("root_id is null")
+	} else {
+		builder = builder.Where("root_id=? or id =?", u.Id, u.Id)
+	}
+	err := builder.Find(&res).Error
+	return res, err
+
+}
+
+func (c *GORMCommentDAO) Delete(ctx context.Context, u Comment) error {
+	return c.db.WithContext(ctx).Delete(&Comment{
+		Id: u.Id,
+	}).Error
+}
---
+++ webook/comment/service/comment.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"Book_Exp/webook/comment/domain"
+	"Book_Exp/webook/comment/repository"
+	"context"
+)
+
+type commentService struct {
+	repo repository.CommentRepository
+}
+
+func NewCommentService(repo repository.CommentRepository) CommentService {
+	return &commentService{repo: repo}
+}
+
+func (c *commentService) GetMoreReplies(ctx context.Context,
+	rid int64,
+	maxID int64, limit int64) ([]domain.Comment, error) {
+	return c.repo.GetMoreReplies(ctx, rid, maxID, limit)
+}
+
+func (c *commentService) GetCommentList(ctx context.Context, biz string,
+	bizId, minID, limit int64) ([]domain.Comment, error) {
+	list, err := c.repo.FindByBiz(ctx, biz, bizId, minID, limit)
+	if err != nil {
+		return nil, err
+	}
+	return list, err
+}
+
+func (c *commentService) DeleteComment(ctx context.Context, id int64) error {
+	return c.repo.DeleteComment(ctx, domain.Comment{
+		Id: id,
+	})
+}
+
+func (c *commentService) CreateComment(ctx context.Context, comment domain.Comment) error {
+	return c.repo.CreateComment(ctx, comment)
+}
